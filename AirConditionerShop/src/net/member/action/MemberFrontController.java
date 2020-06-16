aadf2dc73c66e166b1785cb5e3a7250ee464c953
seeeeo <cheol291330@gmail.com>
2020-06-16 17:52:52 +0900

    ACS 2020.06.16 MemberFrontController 수정 index.jsp 실행 시 Main.jsp 로 이동 처리

--- AirConditionerShop/src/net/member/action/MemberFrontController.java
+++ AirConditionerShop/src/net/member/action/MemberFrontController.java
@@ -2,6 +2,7 @@ package net.member.action;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +17,53 @@ public class MemberFrontController extends HttpServlet {
 		System.out.println("doProcess 호출");
 		
 		//가상 주소 비교해서 처리
+		String requestURI = request.getRequestURI();
+		// = reqest.getRequestURI().toString();
+		
+		System.out.println("URI : "+requestURI);
+		
+		//프로젝트 명 가져오기
+		String contextPath = request.getContextPath();
+		System.out.println("ContextPath : "+contextPath);
+		
+		//실제 변경되는 가상 주소
+		String command = requestURI.substring(contextPath.length());
+		System.out.println("Command : "+command);
+		System.out.println("@@@@ 주소 계산 완료 @@@@");
+		////////////////////////////////////////////////////////////////////
+		//계산된 가상 주소와 특정 페이지가 같으면 페이지의 동작에 따라서 이동
+		
+		//처리 페이지에 대한 정보 객체(인터페이스 - execute())
+		Action action = null;
+		
+		//페이지 이동 정보 저장 객체
+		ActionForward forward = null;
+		
+		
+		//메인 페이지 처리
+		if(command.equals("/Main.me")){
+			//메인페이지 이동 처리
+			// ./main.jsp 이동
+			forward = new ActionForward();
+			forward.setPath("./main.jsp");
+			forward.setRedirect(false);
+		}
+		
+		//페이지 이동 처리
+		if(forward != null){
+			//페이지 이동 정보가 있을 때
+			
+			//페이지 이동 sendRedirect/forward
+			if(forward.isRedirect()){
+				//true
+				response.sendRedirect(forward.getPath());
+			}else{
+				//false
+				RequestDispatcher dis = request.getRequestDispatcher(forward.getPath());
+				dis.forward(request, response);
+			}
+		}
+		
 	}
 	
 	
